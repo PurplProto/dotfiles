c7a40cb7f305a4fd89a8a806c120477c052f065c
PairedPrototype <pairedprototype@hotmail.com>
2019-01-24 14:42:21 +0000

    Now creates the Git ps1 link as well

--- create-links.sh
+++ create-links.sh
@@ -2,17 +2,27 @@
 
 projectLocation=$(pwd)
 
-fileNames=(
+homeFiles=(
 	.bashrc
 	.bash_ps
 	.gitconfig
 	.vimrc
 )
 
-for FILE in ${fileNames[@]}
+depFiles=(
+    .git-prompt.sh
+)
+
+for FILE in ${homeFiles[@]}
 do
-	mv ~/$FILE ~/$FILE.bak
+	mv ~/$FILE ~/$FILE.bak 
 	ln -s $projectLocation/configs/home/$FILE ~/$FILE
 done
 
+for FILE in ${depFiles[@]}
+do
+	ln -s $projectLocation/dependencies/$FILE ~/$FILE
+done
+
 echo "Now type \"exec .bashrc\""
+
